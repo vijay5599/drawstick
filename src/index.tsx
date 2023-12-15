3aaa0fc1e63aa1665242178ebfde5277e39c12e1
vijay5599 <vbvijay84@gmail.com>
2023-12-15 14:10:20 +0530

    new

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
-import * as React from "react";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./styles.css";
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );

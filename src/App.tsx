b70b97df33a681016740d04e40431ed1b4350674
vijay5599 <vbvijay84@gmail.com>
2023-12-15 13:58:34 +0530

    new app file added

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BasicExample from "./BasicExample";
+
+function App() {
+  return (
+    <div>
+      <BasicExample />
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,14 @@
+import * as React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import {
-	DefaultErrorFallback,
-	ErrorBoundary,
-} from '@tldraw/tldraw'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import BasicExample from './BasicExample'
+import App from "./App";
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-function App() {
-	return (
-		<div className="examples">
-			<BasicExample/>
-		</div>
-	)
-}
-
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-	}
-])
-
-document.addEventListener('DOMContentLoaded', () => {
-	const rootElement = document.getElementById('root')!
-	const root = createRoot(rootElement!)
-	root.render(
-		<StrictMode>
-			<ErrorBoundary
-				fallback={(error) => <DefaultErrorFallback error={error} />}
-				onError={(error) => console.error(error)}
-			>
-				<RouterProvider router={router} />
-			</ErrorBoundary>
-		</StrictMode>
-	)
-})
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
